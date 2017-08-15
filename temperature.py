b17876c4ef800b7418bd4c87668d519851a99634
Yan <nezd@protonmail.com>
2017-08-15 18:11:30 +0300

    /temp command

---
+++ temperature.py
@@ -0,0 +1,40 @@
+import octeon
+PLUGINVERSION = 2
+# Always name this variable as `plugin`
+# If you dont, module loader will fail to load the plugin!
+plugin = octeon.Plugin()
+
+def convert(temp, unit):
+    unit = unit.lower()
+    if unit == "c":
+        temp = 9.0 / 5.0 * temp + 32
+        return "%s degrees Fahrenheit"% round(temp, 2)
+    if unit == "f":
+        temp = (temp - 32)  / 9.0 * 5.0
+        return "%s degrees Celsius"% round(temp, 2)
+
+@plugin.command(command="/temp",
+                description="Converts temparture",
+                inline_supported=True,
+                hidden=False)
+def temperatures(bot, update, user, args):
+    """
+    Converts temp from Celsius to Fahrenheit and Backwards
+    Example usage:
+    User:/temp 3C
+    Bot:37.4 degrees Fahrenheit
+    User:/temp 37.4F
+    Bot:3.0 degrees Celsius
+    """
+    if len(args) > 0:
+        out = ""
+        if args[0].upper().endswith("C") or args[0].upper().endswith("F"):
+            unit = args[0][-1]
+            temp = float(args[0][:-1])
+        else:
+            unit = args[-1]
+            temp = float(args[0])
+        out = convert(temp, unit)
+        return octeon.message(text=out)
+    else:
+        return octeon.message("No arguments supplied")
