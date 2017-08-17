10f40d6d3b1624762934c1a25e76b89189865768
Yan <nezd@protonmail.com>
2017-08-17 13:22:35 +0300

    Updated /ud to new format

--- urban.py
+++ urban.py
@@ -2,7 +2,7 @@ from telegram.ext import Updater, CommandHandler
 from telegram import Bot, Update
 from requests import get
 from html import escape
-import constants
+import octeon
 apiurl = "http://api.urbandictionary.com/v0/define"
 message = """
 Definition for <b>%(word)s</b> by %(author)s:
@@ -14,13 +14,14 @@ Examples:
 </i>
 <a href="%(permalink)s">Link to definition on Urban dictionary</a>
 """
-
-
-def preload(*_):
-    """Unused"""
-    return
-
-
+PLUGINVERSION = 2
+# Always name this variable as `plugin`
+# If you dont, module loader will fail to load the plugin!
+plugin = octeon.Plugin()
+@plugin.command(command="/ud",
+                description="Searches for definition of specfied word in urban dictionary",
+                inline_supported=True,
+                hidden=False)
 def urband(_: Bot, ___: Update, user, args):
     """/ud"""
     definition = get(apiurl, params={
@@ -29,16 +30,7 @@ def urband(_: Bot, ___: Update, user, args):
     if definition["result_type"] == "exact":
         definition = definition["list"][0]
         msg = message % definition
-        return msg, constants.HTMLTXT
+        return octeon.message(msg, parse_mode="HTML")
     else:
-        return "Nothing found!", constants.TEXT, "failed"        
-
-COMMANDS = [
-    {
-        "command":"/ud",
-        "function":urband,
-        "description":"Searches for definition of specfied word in urban dictionary",
-        "inline_support":True
-    }
-]
+        return octeon.message("Nothing found!", failed=True)
 
