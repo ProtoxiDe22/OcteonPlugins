80f6dacb7ca855aa2aca0fcad641138fbdab9a7f
Yan <nezd@protonmail.com>
2017-08-27 18:39:05 +0300

    Fixes in /about

--- about.py
+++ about.py
@@ -6,10 +6,11 @@ import octeon
 PLUGINVERSION = 2
 plugin = octeon.Plugin()
 ABOUT_STRING = "Octeon-PTB based on commit <code>" + subprocess.check_output('git log -n 1 --pretty="%h"', shell=True).decode('utf-8') + "</code>"
-ABOUT_STRING += "Octeon-Core based on commit <code>" + subprocess.check_output('git log -n 1 --pretty="%h" -C octeon', shell=True).decode('utf-8') + "</code>"
-ABOUT_STRING += "Octeon-Plugins based on commit <code>" + subprocess.check_output('git log -n 1 --pretty="%h" -C plugins', shell=True).decode('utf-8') + "</code>"
+ABOUT_STRING += subprocess.check_output('git --git-dir octeon/.git log -n 1 --pretty=" Octeon-Core based on commit <code>%h</code>"', shell=True).decode('utf-8')[1:]
+ABOUT_STRING += subprocess.check_output('git --git-dir plugins/.git log -n 1 --pretty=" Octeon-Plugins based on commit <code>%h</code>"', shell=True).decode('utf-8')[1:]
 ABOUT_STRING += "Python-Telegram-Bot version: <code>" + telegram.__version__ + "</code>\n"
 ABOUT_STRING += "Running on <code>" + platform.platform() + "</code>." + "\nPython: <code>" + platform.python_implementation() + " " + platform.python_version() + "</code>\n"
+ABOUT_STRING += '<a href="github.com/octonezd/octeon">GitHub page</a>\n'
 ABOUT_STRING += ABOUT_TEXT
 @plugin.command(command="/about",
                 description="About bot",
