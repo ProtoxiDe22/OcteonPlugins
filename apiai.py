3750f58a316c7c34f59cc35ef951f963a3d920ee
Yan <nezd@protonmail.com>
2017-09-24 17:04:45 +0300

    Allow replies for AI

--- apiai.py
+++ apiai.py
@@ -6,6 +6,17 @@ import random
 PLUGINVERSION = 2
 assert settings.API_AI_TOKEN != ""
 ai = apiai.ApiAI(settings.API_AI_TOKEN)
+def handle_ai(bot, update):
+    req = ai.text_request()
+    req.session_id = random.randint(0, 9999999999)
+    req.query = update.message.text
+    resp = json.loads(req.getresponse().read().decode("utf-8"))
+    if not resp["result"]["action"].startswith("octobot"):
+        return core.message(text=resp["result"]["fulfillment"]["speech"])
+    else:
+        ai_resp = bot.modloader.handle_ai(update, resp["result"]["action"])
+        if ai_resp:
+            return ai_resp(bot, update, resp)
 # Always name this variable as `plugin`
 # If you dont, module loader will fail to load the plugin!
 plugin = core.Plugin()
@@ -15,13 +26,7 @@ def apiai_provider(bot, update):
     react.append(bot.getMe().first_name)
     for pseudoname in react:
         if pseudoname.lower() in update.message.text.lower():
-            req = ai.text_request()
-            req.session_id = random.randint(0, 9999999999)
-            req.query = update.message.text
-            resp = json.loads(req.getresponse().read().decode("utf-8"))
-            if not resp["result"]["action"].startswith("octobot"):
-                return core.message(text=resp["result"]["fulfillment"]["speech"])
-            else:
-                ai_resp = bot.modloader.handle_ai(update, resp["result"]["action"])
-                if ai_resp:
-                    return ai_resp(bot, update, resp)
+            return handle_ai(bot, update)
+    if update.message.reply_to_message:
+        if update.message.reply_to_message.from_user == bot.getMe():
+            return handle_ai(bot, update)
